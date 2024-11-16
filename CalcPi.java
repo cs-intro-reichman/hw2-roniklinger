2187cd3243381707502e16850d4b36f0e4e41b73
roniklinger <roni.klinger@post.runi.ac.il>
2024-11-16 11:35:49 +0200

    check CalcPi 1

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,13 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		double approx = 0;
+		for(int i = 0; i < num ; i++){
+			approx = approx + (Math.pow(-1, i) / (2 * i + 1));
+			
+		}
+		System.out.println(Math.PI);
+		System.out.println(approx*4);
 	}
 }
